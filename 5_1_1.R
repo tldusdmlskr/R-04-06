651a9db0d0c6c09f45909f473a9108b889a60b87
tldusdmlskr <2373012@ewhain.net>
2025-03-25 18:14:42 +0900

    all

---
+++ 5_1_1.R
@@ -0,0 +1,10 @@
+exam <- read.csv("csv_exam.csv")
+
+head(exam)
+head(exam, 10)
+tail(exam)
+tail(exam, 10)
+View(exam)
+dim(exam)
+str(exam)
+summary(exam)
---
+++ 5_1_2.R
@@ -0,0 +1,11 @@
+install.packages("ggplot2")
+library(ggpolt2)
+
+mpg <- (as.data.frame(ggplot2::mpg))
+
+head(mpg)
+tail(mpg)
+View(mpg)
+dim(mpg)
+str(mpg)
+summary(mpg)
