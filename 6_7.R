cd89308872b4e2c1434b12ecd458b38ed06def65
tldusdmlskr <2373012@ewhain.net>
2025-03-28 14:15:17 +0900

    Create 6_7.R

---
+++ 6_7.R
@@ -0,0 +1,14 @@
+#중간고사 데이터 생성
+test1 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    midterm = c(60, 80, 70, 90, 85))
+
+#기말고사 데이터 생성
+test2 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    final = c(70, 83, 65, 95, 80))
+test1 #test1 출력
+test2 #test2 출력
+
+total <- left_join(test1, test2, by = "id")
+total
+
+name
